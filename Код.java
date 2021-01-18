63516c5058cb036cdabf444896bdaff8e942308f
Екатерина <76491846+EkaterinaPeregudova@users.noreply.github.com>
2021-01-18 20:18:09 +0300

    Update Код.java

--- Код.java
+++ Код.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String[] args) {
         int b = 2_000_000_000;
         int p = 500_000_000;
-        int rest = balanse + pay;
+        int rest = b + p;
                 System.out.print(rest);
     }
 }
